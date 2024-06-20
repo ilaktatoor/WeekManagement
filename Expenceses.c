c9c06a46a66b95415fd3c2f0579045a2674ab84c
ilaktatoor <cemmnuelaxa@gmail.com>
2024-06-20 16:06:59 -0600

    frame

---
+++ Expenceses.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct{
+
+}Expense;
+
+typedef struct{
+
+}Week;
+
+void addExpense(){
+
+}
+
+
+int main(){
+
+return 0;
+
+}
