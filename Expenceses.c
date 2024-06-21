d40e5ec4154d16a182e6d2eb0ccfef0370a1cbbe
ilaktatoor <cemmnuelaxa@gmail.com>
2024-06-20 20:22:00 -0600

    basic structures adding expenses

--- Expenceses.c
+++ Expenceses.c
@@ -5,7 +5,19 @@
 
 
 int main(){
+  Week semana1;
+  strcpy(semana1.name, "semana1");
+  semana1.expenses=NULL;
+  semana1.num_expenses = 0;
 
-return 0;
+  addExpense(&semana1,"wallmart",1576.89);
+  addExpense(&semana1,"heb",150.00);
+
+  for(int i = 0; i<semana1.num_expenses; i++ ){
+    printf("Expense %d: %s, %.2f\n",i + 1,week.expenses[i].description,week.expenses[i].amount);  
+  }
+
+  free(semana1.expenses);
+  return(0);
 
 }
