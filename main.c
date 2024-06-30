8fd1a2389e0aff8ac2e19b588d95400c089eec89
ilaktatoor <cemmnuelaxa@gmail.com>
2024-06-29 18:26:56 -0600

    menu in main.c

--- main.c
+++ main.c
@@ -8,38 +8,61 @@
 int main(){
   sqlite3 *db;
   initialize_db(&db);
-  create_table_if_not_exist(db);
+  
+  while (1) {
+    printf("\n 1. Add week\n 2. Select week to add expense\n3. Edit expense\n4. Show expenses of a week\n5. List weeks\n6. Exit\n");
+    int choise;
+    printf("Enter your choise:");
+    scanf("%d",&choise);
+    getchar();
 
-  WeekClass semana1;
-  get_input("Enter the name of the week: ", semana1.name, sizeof(semana1.name));
-  semana1.expenses = NULL;
-  semana1.num_expenses = 0;
+    if(choise == 1){
+      //Add Week
+      char week_name[MAX_INPUT_SIZE];
+      get_input("Enter name of the week", week_name, sizeof(week_name));
+      insert_week_to_db(db, week_name);
 
-  char description[MAX_INPUT_SIZE];
-  float amount;
+    }else if(choise == 2) {
+      //week to add expense
+      int week_id;
+      printf("Enter week id to add expenses: ");
+      scanf("%d",&week_id);
+      getchar();
 
-  while (1){
-    get_input("Enter expense description (or 'exit' to finish): ", description, sizeof(description));
+      char description[MAX_INPUT_SIZE];
+      float amount;
+      get_input("Enter expense description", description, sizeof(description));
+      printf("Enter expense amount:$ ");
+      scanf("%f",&amount);
+      getchar();
 
-    if (strcmp(description, "exit")==0) {
-      break;    
+      insert_week_to_db(db, week_id,description, amount);
+    
+    }else if(choise == 3){
+      //Edit expenses
+
+    }else if(choise == 4){
+      //show expenses of a week
+      int week_id;
+      printf("Enter Week id to show expenses: ");
+      scanf("%d",&week_id);
+      getchar();
+
+      show_expenses_of_week(db, week_id);
+
+    }else if(choise == 5){
+      //list weeks
+      list_weeks(db);
+    }else if(choise == 6){
+      //exit
+      break;
+    }else{
+      printf("[!] Invalid choise. try again");
     }
-
-    printf("Enter expense amount: ");
-    scanf("%f",&amount);
-
-    while(getchar() != '\n');
-
-    addExpense(&semana1, description, amount);
-    insert_expense(db, description, amount);
-
+  
   }
-  for(int i = 0; i< semana1.num_expenses; i++){
-    printf("Expense %d: %s, %.2f\n",i+1,semana1.expenses[i].description,semana1.expenses[i].amount);
-
-  }
-  free(semana1.expenses);
-  sqlite3_close(db);
+  
+  close_db(db);
   return 0;
 }
 
